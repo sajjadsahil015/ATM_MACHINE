fa0f343f6d390c287674d9a43eab7af63af3d908
Sajjad <sajjadsahil015@gmail.com>
2024-03-27 22:03:49 +0500

    first commit

---
+++ atm.js
@@ -0,0 +1,67 @@
+import inquirer from "inquirer";
+let myBalance = 10000;
+let myPin = 1234;
+console.log("Total balance in your account is: " + myBalance);
+let pinAns = await inquirer.prompt({
+    name: "pinNumber",
+    type: "number",
+    message: "Enter Your Pin: ",
+});
+if (pinAns.pinNumber === myPin) {
+    console.log("Your pin is correct!!!");
+    let operationAns = await inquirer.prompt({
+        name: "operations",
+        type: "list",
+        message: "Please select option",
+        choices: ["Withdraw", "Check Balance"],
+    });
+    if (operationAns.operations === "Withdraw") {
+        console.log("You want to withdraw?");
+        let amountAns = await inquirer.prompt({
+            name: "amount",
+            type: "list",
+            message: "Please select amount!",
+            choices: ["1000", "5000", "10000", "Other Amount"],
+        });
+        if (amountAns.amount === "1000") {
+            myBalance -= 1000;
+            //console.log("Your remaining balance is: " + myBalance); you can also use this single line instead of below two lines
+            let newBalance = `${"Your remaining balance is: " + myBalance}`;
+            console.log(newBalance);
+        }
+        else if (amountAns.amount === "5000") {
+            myBalance -= 5000;
+            //console.log("Your remaining balance is: " + myBalance); you can also use this single line instead of below two lines
+            let newBalance = `${"Your remaining balance is: " + myBalance}`;
+            console.log(newBalance);
+        }
+        else if (amountAns.amount === "10000") {
+            myBalance -= 10000;
+            //console.log("Your remaining balance is: " + myBalance); you can also use this single line instead of below two lines
+            let newBalance = `${"Your remaining balance is: " + myBalance}`;
+            console.log(newBalance);
+        }
+        else if (amountAns.amount === "Other Amount") {
+            let otherAmount = await inquirer.prompt({
+                name: "manualAmount",
+                type: "number",
+                message: "Enter the amount: ",
+            });
+            if (otherAmount.manualAmount <= myBalance) {
+                myBalance -= otherAmount.manualAmount;
+                //console.log("Your remaining balance is: " + myBalance); you can also use this single line instead of below two lines
+                let newBalance = `${"Your remaining balance is: " + myBalance}`;
+                console.log(newBalance);
+            }
+            else if (otherAmount.manualAmount > myBalance) {
+                console.log("Insufficient Amount");
+            }
+        }
+    }
+    else if (operationAns.operations === "Check Balance") {
+        console.log("Your Current Balance Is: " + myBalance);
+    }
+}
+else {
+    console.log("Invalid pin");
+}
